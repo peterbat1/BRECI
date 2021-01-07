88055492952a7c2764a7ef736cedde7afa912bbe
Peter D Wilson <peterdonaldwilson@gmail.com>
2021-01-07 16:54:34 +1100

    Made example in help match use of cbPalette()

--- R/BRECI.R
+++ R/BRECI.R
@@ -73,17 +73,15 @@
 #' }
 #'
 #' @examples
+#' \dontrun{
 #' data(breciRasters)
 #'
 #' # Default plot
 #' BRECIplot(ras1, ras2, plotTitle = "Acacia linifolia", outFilename = "~/testPlot.png")
 #'
-#' # A colour-blind friendly palette derived from info found here:
-#' # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
-#' # last accessed 2020-03-14
-#' colTable <- matrix(c(213,94,0,204,121,167,240,228,66,0,158,115,0,114,178), 5, 3, byrow = TRUE)/256
-#' niceColours <- rev(rgb(colTable))
-#' BRECIplot(ras1, ras2, plotTitle = "Acacia linifolia", outFilename = "~/testPlot.png", binCols = niceColours)
+#' # Use a colour-blind friendly palette:
+#' BRECIplot(ras1, ras2, plotTitle = "Acacia linifolia", outFilename = "~/testPlot.png", binCols = cbPalette())
+#' }
 #'
 BRECIplot <- function(ras1 = NULL, ras2 = NULL, outFilename = NULL, saveToFile = TRUE,
                       plotTitle = "", plotWidth = 275, plotHeight = 200,
